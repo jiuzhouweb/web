1757336600b750764fb9436c62680c46d282b507
miaoxing <25910323@qq.com>
2019-06-27 17:36:46 +0800

    modify

--- src/views/audit.vue
+++ src/views/audit.vue
@@ -5,7 +5,7 @@
 				<span class="labelTitle">
 					客户名称：
 				</span>
-				<el-select v-model="searchList.value" placeholder="请选择">
+				<el-select v-model="searchList.customerId" placeholder="请选择">
 					<el-option v-for="item in $store.state.cust" :key="item.customerId" :label="item.customerName" :value="item.customerId">
 					</el-option>
 				</el-select>
@@ -14,13 +14,30 @@
 				<span class="labelTitle">
 					账期：
 				</span>
-				<el-date-picker v-model="searchList.nowDate" type="month" placeholder="选择月">
+				<el-date-picker v-model="searchList.period" type="month" placeholder="选择月" value-format="yyyy-MM">
 				</el-date-picker>
 			</div>
 			<div class="searchButton" @click="search()">查询</div>
 		</div>
 		<div class='body_contain'>
-			<div class='card' v-for="item in 5">
+			<div class='card' v-for="item in taxTreatment">
+				<div class='card_head'>
+					<p>南京测试客户</p>
+					<p>2018-12</p>
+					<p>大额审核</p>
+					<p>税金：800000元</p>
+				</div>
+				<div class='card_body'>
+					<p>责任会计：aaa</p>
+					<p>工号：bbb</p>
+					<p>税务类型：ccc</p>
+				</div>
+				<div class='card_btn'>
+					<el-button>发回重做</el-button>
+					<el-button>审核通过</el-button>
+				</div>
+			</div>
+			<div class='card' v-for="item in bigTaxTreatment">
 				<div class='card_head'>
 					<p>南京测试客户</p>
 					<p>2018-12</p>
@@ -48,12 +65,71 @@
 			return {
 				message: "12334456",
 				searchList: {
-					value: "",
-					nowDate: ""
+					customerId: "",
+					period: ""
 				},
+				bigTaxTreatment:[],
+				TaxTreatment:[]
 			}
 		},
-		components: {}
+		methods:{
+			search(){
+				this.queryTaxTreatment();
+				this.queryBigTaxTreatment();
+			},
+			queryTaxTreatment(){
+				let params = this.searchList;
+				this.axios.post('/perTaxToolTwo/e9z/taxTreatment/selectTaxTreatment', this.qs.stringify(params), {
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+					}
+				}).then(res => {
+					if (res.data.code == 200) {
+						this.taxTreatment = res.data.data;
+						
+					} else {
+						this.$message({
+							message: res.data.msg,
+							type: 'error'
+						});
+					}
+				
+				}).catch(function(err) {
+					this.$message({
+						message: '获取税款审核数据失败',
+						type: 'error'
+					});
+				})
+			},
+			queryBigTaxTreatment(){
+				let params = this.searchList;
+				this.axios.post('/perTaxToolTwo/e9z/taxTreatment/selectBigTaxTreatment', this.qs.stringify(params), {
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+					}
+				}).then(res => {
+					if (res.data.code == 200) {
+						this.bigTaxTreatment = res.data.data;
+						
+					} else {
+						this.$message({
+							message: res.data.msg,
+							type: 'error'
+						});
+					}
+				
+				}).catch(function(err) {
+					this.$message({
+						message: '获取税款审核数据失败',
+						type: 'error'
+					});
+				})
+			}
+		},
+		components: {},
+		created(){
+			
+		}
 	}
 </script>
 
@@ -87,6 +163,50 @@
 			margin-right: .2rem;
 			background: #fff;
 			height: 4.85rem;
+			.card_head{
+				height: 2rem;
+				box-sizing: border-box;
+				padding-left: 0.2rem;
+				color: #fff;
+				padding-top: 0.3rem;
+				background: url(../assets/img/list-bg1.png) no-repeat;
+				p:nth-of-type(1){
+					font-size: 0.18rem;
+					height: 0.36rem;
+					line-height: 0.36rem;
+				}
+				p:nth-of-type(2){
+					font-size: 0.12rem;
+					height: 0.3rem;
+					line-height: 0.3rem;
+				}
+				p:nth-of-type(3){
+					font-size: 0.16rem;
+					height: 0.34rem;
+					line-height: 0.34rem;
+				}
+				p:nth-of-type(4){
+					font-size: 0.16rem;
+					height: 0.38rem;
+					line-height: 0.38rem;
+				}
+			}
+			.card_body{
+				margin: 0.2rem 0;
+				p{
+					height: 0.3rem;
+					line-height: 0.3rem;
+					font-size:0.14rem;
+					padding-left: 0.2rem;
+					color: #666
+				}
+				p:nth-of-type(1){
+				}
+				p:nth-of-type(2){
+				}
+				p:nth-of-type(3){
+				}
+			}
 		}
 		.card:nth-of-type(5n){
 			margin-right: 0rem;
